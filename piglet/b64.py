b9392868dd92ac271c224ae605afdd70cb1b3713
Tagir <khalilov.tg@gmail.com>
2023-01-16 07:02:45 +0300

    Rebuild basement of b64 class

--- piglet/b64.py
+++ piglet/b64.py
@@ -4,27 +4,28 @@ import base64
 class B64:
     '''Class to encode/decode files content and write/read to objects files'''
 
-    def __init__(self, file: str):
-        self.file = file
-
-
-
-
-    '''
-        * Или сделать подкласс!
-    '''
-
-
-
-
-
-
-    def encode(self):
-        with open(self.file, 'r') as f:
-            encoded = base64.encode(f.read().encode('utf-8'))
-
-        def save_to_file(save_file: str):
+    def __init__(self):
+        ...
+        
+    class B64String:
+        
+        string: str
+        
+        def __init__(self, string: str):
+            self.string = string
+            
+        def __str__(self):
+            return self.string
+            
+        def save_to_file(self, save_file: str):
             with open(save_file, 'w') as f:
-                f.write(encoded)
-
-        return encoded
+                f.write(self.string)
+            
+        
+    @staticmethod
+    def encode(file: str):
+        with open(file, 'r') as f:
+            encoded = base64.b64encode(f.read().encode('utf-8')).decode('utf-8')
+            
+            return B64.B64String(encoded)
+    
